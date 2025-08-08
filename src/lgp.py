13830d482ced0cf0857f04b649971dab45e58a0b
ZhixingH <41175145+Zhixing1020@users.noreply.github.com>
2025-08-08 13:07:08 +1200

    update

--- src/lgp.py
+++ src/lgp.py
@@ -12,58 +12,17 @@ Use the ideas of ECJ to use and reimplement the functions of DEAP.
 
 # import classes and files
 import sys
-sys.path.append('D:/zhixing/科研/LGP4PY/LGP4PY')
+sys.path.append('D:/data/study/LGP4PY/LGP4PY')
 from src.ec import *
 from src.ec.util import *
-from tasks.Symbreg.optimization.gp_symbolic_regression import GPSymbolicRegression
+# from tasks.Symbreg.optimization.gp_symbolic_regression import GPSymbolicRegression
 
 # set up LGP behaviors
 '''read parameters from the parameter file and set it into pset and toolbox'''
 
 # define main() and run main()  
 
+
+
 if __name__ == "__main__":
-    
-    state = EvolutionState('D:\\zhixing\\科研\\LGP4PY\\LGP4PY\\tasks\\Symbreg\\parameters\\LGP_test.params')
-    # state.setup("")
-    state.run()  # run the evolution state, which will read the parameters and set up the environment
-
-    # builder = GPBuilder()
-    # builder.setup(state, Parameter("gp.koza.half"))
-    # state.primitive_set = GPPrimitiveSet()
-    # state.primitive_set.setup(state, Parameter('gp.fs.0'))  # here, I need to use a default parameter name
-    # # fun_set = {Add(), InputFeatureGPNode()}
-    
-    # tree = GPTree()
-    # for _ in range(0,10):
-    #     tree.child = builder.newRootedTree(state, 0, tree, state.primitive_set, 0)
-    #     print(str(tree) + "\t\t" + str(tree.child.numNodes(GPNode.NODESEARCH_ALL)) + "\t\t" + str(tree.child.numNodes(GPNode.NODESEARCH_NONTERMINALS)))
-    
-    # individual = GPIndividual()
-    # individual.setup(state, Parameter('pop.subpop.0.species.ind'))
-    # individual.getTree(0).child = builder.newRootedTree(state, 0, individual.getTree(0), state.primitive_set, 0)
-    # individual.fitness = Fitness()
-
-    # print( individual.printIndividualForHuman() )
-
-    # species = GPSpecies()
-    # species.setup(state, Parameter('pop.subpop.0.species'))
-
-    # subpop = Subpopulation()
-    # subpop.setup(state, Parameter('pop.subpop.0'))
-    # subpop.populate(state, 0)
-
-    pop = Population()
-    pop.setup(state, Parameter('pop'))
-    pop.populate(state, 0)
-
-    prob = GPSymbolicRegression()
-    prob.setup(state, Parameter('eval.problem'))
-
-    for subpop in pop.subpops:
-        for ind in subpop.individuals:
-            prob.evaluate(state, ind, subpop, 0)
-
-    # for tree in subpop.individuals:
-    #     print(tree)
-    print("stop")
+    Evolve.main()
